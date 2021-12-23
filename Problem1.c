a4c7427bcb7aca928222623ed679209abcc3cb1a
Cristi_an <cristianpopescu0426@gmail.com>
2021-12-24 01:44:36 +0200

    Mario problem
    week 1

---
+++ Problem1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+{ 
+    int n;
+    do
+    {
+     //take user input 
+     n= get_int("Height: " );
+    
+    } while (n < 1 || n > 8);
+    
+    
+         
+            for(int i = 0; i < n ; i++)
+            {
+                for (int  j = 0; j < n  ; j++)
+                {
+                    if(i + j < n - 1 )
+                    printf(" ");
+                    else
+                    printf("#");
+            
+                    
+                    }
+                
+                printf("\n");
+                }
+           
+            
+        
+    
+    
+    
+    
+    
+    
+}
+   
+    
+ 
